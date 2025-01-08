60d76a335c63905de0fe43bd67a6a479e0c5f702
adityabanik2004 <adityabanik2004@gmail.com>
2025-01-08 11:53:22 +0530

    Update test10.java

--- test10.java
+++ test10.java
@@ -1,28 +1,39 @@
-import java.util.Scanner;
+class test10 {
+    public static void main(String[] args) {
+        Properties(11);                     
+    }
 
-public class test10 {
-
-    public static boolean isPrime(int num) {
-        if (num <= 1) {
-            return false; // Numbers less than or equal to 1 are not prime
+     static void Properties(int number) {
+        if (Prime(number)) {                     
+            System.out.println(number + " is a prime number");
+        } else {
+            System.out.println(number + " is not a prime number");
         }
-        for (int i = 2; i * i <= num; i++) { // Optimized for loop
+
+        if (Odd(number)) {
+            System.out.println(number + " is an odd number");
+        } else {
+            System.out.println(number + " is not an odd number");
+        }
+    }
+
+    static boolean Prime(int num) {
+        if (num <= 1) {            
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(num); i++) {
             if (num % i == 0) {
-                return false; // If divisible by any number other than 1 and itself, it's not prime
+                return false;
             }
         }
-        return true; // If no divisors found, it's prime
+        return true;
     }
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter a number: ");
-        int number = scanner.nextInt();
-
-        if (isPrime(number)) {
-            System.out.println(number + " is prime and " + (number % 2 != 0 ? "odd" : "even"));
-        } else {
-            System.out.println(number + " is not prime.");
-        }
-    }
+    static boolean Odd(int num) {
+         if(num%2!=0){            //Method for checking odd
+			 
+		 }
+		 return true;
+	}
 }
+ 
