6b3c66c295b9a6e0c41f101ad1718ed91b0dc32b
adityabanik2004 <adityabanik2004@gmail.com>
2025-01-08 11:12:29 +0530

    Update test3.java

--- test3.java
+++ test3.java
@@ -5,26 +5,28 @@ public class test3 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Get the first two numbers
         System.out.print("Enter the first number: ");
         int num1 = scanner.nextInt();
+
         System.out.print("Enter the second number: ");
         int num2 = scanner.nextInt();
 
-        // Find the largest of the two numbers
-        int largest = (num1 > num2) ? num1 : num2;
-
-        // Get the third number
         System.out.print("Enter the third number: ");
         int num3 = scanner.nextInt();
 
-        // Calculate the sum of the first two numbers
-        int sum = num1 + num2;
+        int smallest = findSmallest(num1, num2, num3); 
 
-        // Find the smallest of the sum and the third number
-        int smallest = (sum < num3) ? sum : num3;
-
-        // Print the smallest number
         System.out.println("The smallest number is: " + smallest);
+    }
+
+    public static int findSmallest(int a, int b, int c) {
+        int smallest = a; 
+        if (b < smallest) {
+            smallest = b;
+        }
+        if (c < smallest) {
+            smallest = c;
+        }
+        return smallest;
     }
 }
