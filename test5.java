18facabe6b25360e7fab1de4c86c2283c3a0536e
adityabanik2004 <adityabanik2004@gmail.com>
2025-01-08 11:18:12 +0530

    Update test5.java

--- test5.java
+++ test5.java
@@ -1,12 +1,15 @@
 public class test5 {
 
     public static void main(String[] args) {
-        int sum = 0; 
+        int sum = EvenNumbers(2, 20); 
+        System.out.println("Sum of even numbers from 1 to 20: " + sum);
+    }
 
-        for (int i = 2; i <= 20; i += 2) { 
-            sum += i; 
+    public static int EvenNumbers(int start, int end) {
+        int sum = 0;
+        for (int i = start; i <= end; i += 2) {
+            sum += i;
         }
-
-        System.out.println("Sum of even numbers from 1 to 20: " + sum); 
+        return sum;
     }
 }
