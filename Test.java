6e92b666eb91779802f5be80d3f16e835c7d506e
adityabanik2004 <adityabanik2004@gmail.com>
2025-01-08 10:08:09 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,21 +1,18 @@
-public class test 
-{
-    public static void main(String[] args) 
-	{
-        for (int i = 1; i <= 7; i++) 
-		{
-            if (i == 3) 
-			{
-                System.out.println("I am 3");
-            } 
-			else if (i == 5) 
-			{
-                System.out.println("I am 5");
-            } 
-			else 
-			{
-                System.out.println(i);
-            }
+public class Test {
+
+    public static String checkNumber(int i) {
+        if (i == 3) {
+            return "I am 3";
+        } else if (i == 5) {
+            return "I am 5";
+        } else {
+            return String.valueOf(i); 
+        }
+    }
+
+    public static void main(String[] args) {
+        for (int i = 1; i <= 7; i++) {
+            System.out.println(checkNumber(i));
         }
     }
 }
